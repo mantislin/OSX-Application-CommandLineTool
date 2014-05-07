2eb6cf1534bee28fd0a4446aeb5156f8d908a6cd
mantislin <mntslin@gmail.com>
2014-05-07 11:33:54 +0800

    Initial Commit

---
+++ OSX-Application-CommandLineTool/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  OSX-Application-CommandLineTool
+//
+//  Created by Sword on 5/7/14.
+//  Copyright (c) 2014 Sword. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
