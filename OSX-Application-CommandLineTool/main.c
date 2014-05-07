b1b447d418239b86298d546657be344d7593d0e0
mantislin <mntslin@gmail.com>
2014-05-07 18:20:01 +0800

    ACM Funning

--- OSX-Application-CommandLineTool/main.c
+++ OSX-Application-CommandLineTool/main.c
@@ -7,12 +7,35 @@
 //
 
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(int argc, const char * argv[])
-{
+#define Max 1000
 
-    // insert code here...
-    printf("Hello, World!\n");
+int main(int argc, const char * argv[]){
+    char a[Max + 1] = "0", b[Max + 1] = "0", c[Max + 1] = "1";
+    int T;
+    while (scanf("%d", &T) != EOF) {
+        int i;
+        for (i = 0; i < T; i++) {
+            int alen, blen, clen, ten = 0;
+            scanf("%s%s", a + 1, b + 1);
+        }
+    }
+
+    /* 1001
+    int n;
+    while (scanf("%d", &n) != EOF) {
+        int sum = 0, i;
+        for (i = 0; i <= n; i++) {
+            sum += i;
+        }
+        printf("%d\n\n", sum);
+    }
     return 0;
-}
+     */
 
+    /* 1000
+    int a,b;
+    while(scanf("%d%d",&a,&b)!=EOF)printf("%d\n",a+b);
+r   */
+}
