31690f31d4d8d48182df8413e8cf2fa89c455c07
mantislin <mntslin@gmail.com>
2014-05-07 18:27:18 +0800

    1002
    1002

--- OSX-Application-CommandLineTool/main.c
+++ OSX-Application-CommandLineTool/main.c
@@ -19,8 +19,25 @@ int main(int argc, const char * argv[]){
         for (i = 0; i < T; i++) {
             int alen, blen, clen, ten = 0;
             scanf("%s%s", a + 1, b + 1);
+            alen = strlen(a + 1);
+            blen = strlen(b + 1);
+            clen = alen > blen ? alen : blen;
+            c[clen + 1] = 0;
+            do {
+                char x = a[alen] + b[blen] - '0' + ten;
+                ten = x > '9' ? 1 : 0;
+                c[clen] = x > '9' ? x - 10 : x;
+                alen = alen > 0 ? alen - 1 : alen;
+                blen = blen > 0 ? blen - 1 : blen;
+            } while (--clen > 0);
+            c[0] = '0' + ten;
+            printf("Case %d:\n", i + 1);
+            printf("%s + %s = %s\n", a + 1, b + 1, c + !ten);
+            if (i + 1 != T)
+                printf("\n");
         }
     }
+    return 0;
 
     /* 1001
     int n;
